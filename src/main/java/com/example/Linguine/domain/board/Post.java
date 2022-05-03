291d94ace4ce97c370117c743b03e5abf202353b
yeoooo <ruisya330@gmail.com>
2022-05-04 01:57:47 +0900

    Post implementing

--- src/main/java/com/example/Linguine/domain/board/Post.java
+++ src/main/java/com/example/Linguine/domain/board/Post.java
@@ -1,18 +1,16 @@
-package com.example.Linguine.board;
+package com.example.Linguine.domain.board;
 
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
-
 import java.util.Date;
 
 @Getter
-@NoArgsConstructor
-public class Post {
+@RequiredArgsConstructor
+public class Post{
     private final Long Post_ID;
-    private Date Posted_Data;
-    private String Post_Owner;
-    private String Post_Title;
-    private String Contents;
-    public String Category;
+    private final Date Posted_Data;
+    private final String Post_Owner;
+    private final String Post_Title;
+    private final String Contents;
+    public final String Category;
 }
