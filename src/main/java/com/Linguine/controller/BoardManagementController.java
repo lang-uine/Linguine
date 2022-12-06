769d87fb435a63ded5cb39e3bb1ad95055cce0ef
yeoooo <ruisya330@gmail.com>
2022-12-06 23:55:57 +0900

    chore: cleansing code

--- src/main/java/com/Linguine/controller/BoardManagementController.java
+++ src/main/java/com/Linguine/controller/BoardManagementController.java
@@ -9,8 +9,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.persistence.DiscriminatorValue;
-import java.util.List;
 import java.util.Optional;
 
 @Controller
