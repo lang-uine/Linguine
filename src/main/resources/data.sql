c9c813e16d0dde54d83cfa55863ba9a405d31109
yeoooo <ruisya330@gmail.com>
2022-12-15 23:58:05 +0900

    #13 fix: script init activate

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,4 @@
-insert into member (id, register_time, update_time, email, last_login_time, name, nick_name, password, role, spec, user_name)
-values (1L, null, null, 'admin', null, 'admin', 'admin',   '$2a$12$pehci6O4NX2kq50jpyB4dedly5psClcTcGZw1kMtza1xJKnB2awwO', 'ADMIN', NULL, NULL);
+insert into member (id, register_time, update_time, email, last_login_time, count, locked, period, name, nick_name, password, role, spec, user_name)
+values (1L, null, null, 'admin', null, 0, false, null,'admin', 'admin',   '$2a$12$pehci6O4NX2kq50jpyB4dedly5psClcTcGZw1kMtza1xJKnB2awwO', 'ADMIN', NULL, NULL);
+insert into member (id, register_time, update_time, email, last_login_time, count, locked, period, name, nick_name, password, role, spec, user_name)
+values (1L, null, null, 'test', null, 0, false, null,'test', 'test',   '$2a$12$Na/StTFUfKVVde8sXulqDO/wz0ykEmLJW1hxv0XVEIhxtrU6rAxMC', 'USER', NULL, NULL);
